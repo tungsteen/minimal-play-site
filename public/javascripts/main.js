87de60c3c60adcbcc4a7a86ec548a14ce47c3d11
Sergey Bosko <sergey.bosko@mevis.de>
2016-08-15 17:24:06 +0200

    add form and template for ajax post

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,8 +4,26 @@ requirejs.config();
 requirejs(['jquery'], function($) {
     $(document).ready(function() {
         console.log("Document is ready!");
-        $("#addButton").click(function() {
-            console.log("Send post per ajax!");
+        $("#addToDoEntryForm").submit( function( event ) {
+            event.preventDefault();
+
+            var submitForm = $( this ), 
+                textForm = submitForm.find("#textToSubmit").val(), 
+                url = submitForm.attr("action");
+
+            console.log("Send post to ajax! " + textForm);
+
+          /*  
+          // Send the data using post
+          var posting = $.post( url, { s: term } );
+ 
+          // Put the results in a div
+          posting.done(function( data ) {
+              console.log("Send post to ajax! " + textForm);
+              // var content = $( data ).find( "#content" );
+              // $( "#result" ).empty().append( content );
+          });
+          */  
         });
     });
 });
