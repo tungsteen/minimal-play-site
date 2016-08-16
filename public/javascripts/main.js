84ea54b9c0012a2dbd7361422522c4136a56a573
tungsteen <sergeybosko@googlemail.com>
2016-08-16 19:20:10 +0200

    add todolist.js, load jquery and bootstrap through requirejs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,15 @@
-require.config();
+require.config({
+    paths:{
+        jquery: '/assets/lib/jquery/jquery.min',
+        bootstrap: '/assets/lib/bootstrap/js/bootstrap.min'
+    }
+});
 
 
 require(["todolist", "jquery"], function(tdl, $) {
     $(document).ready(function() {
 
-        //tdl.updateToDoList();
+        tdl.updateToDoList();
 
         $("#addToDoEntryForm").submit( function( event ) {
             event.preventDefault();
---
+++ public/javascripts/todolist.js
@@ -0,0 +1,10 @@
+define([], function() {
+    function updateToDoList()
+    {
+        console.log("ToDo List: Update!");
+    }
+
+    return {
+        updateToDoList: updateToDoList
+    }
+});
